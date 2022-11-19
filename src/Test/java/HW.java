68b30470dd1dccd4eb0b4db1b7ee6808f1c6f1e2
Mikhail Turilov <turilovm@gmail.com>
2022-11-19 16:16:22 +0300

    add hw

--- src/Test/java/HW.java
+++ src/Test/java/HW.java
@@ -1,3 +1,19 @@
 public class HW {
+    public static void main(String[] args) {
+        byte number1 = 125;
+        byte number2 = 120;
+        byte sum = (byte) (number1 + number2);
+        System.out.println(sum);
 
+        int a = 50;
+        float b = (float) 10.2;
+        System.out.println(a * b);
+
+        int age = 35;
+        String name = "Михаил";
+        if (age < 65){
+            System.out.println("Идите работать, " + name + ", до пенсии еще далеко");
+        }
+
+    }
 }
